1481a7cd4f17f5906fa2b6db8eecd9dbc0b5232c
ridhwanluthra <ridhwancode@gmail.com>
2016-10-14 13:21:58 +0530

    code for thapar

---
+++ thapar/thapar.ino
@@ -0,0 +1,76 @@
+#include <QTRSensors.h>
+
+#define rightMotorF 5
+#define rightMotorB 4
+#define rightMotorPWM 9
+#define leftMotorF 8
+#define leftMotorB 7
+#define leftMotorPWM 3
+#define stby 6
+
+QTRSensorsRC qtr((unsigned char[]) {A0,4, A1, A2, A3, A4, 2, A5}, 8, 2500);
+ 
+void setup()
+{
+  pinMode(rightMotorF, OUTPUT);
+  pinMode(rightMotorB, OUTPUT);
+  pinMode(rightMotorPWM, OUTPUT);
+  pinMode(leftMotorF, OUTPUT);
+  pinMode(leftMotorB, OUTPUT);
+  pinMode(leftMotorPWM, OUTPUT);
+  pinMode(stby,OUTPUT);
+  Serial.begin(9600);
+  for (int i = 0; i < 100; i++)  // make the calibration take about 5 seconds
+  {
+    qtr.calibrate();
+    delay(20);
+  }
+}
+
+int lastError = 0;
+float kp = 0.1;  // 0.08 // for small = 0.1
+float kd = 5; // 1.0   // for small = 1.7
+float ki = 0;
+int integral = 0;
+int derivative = 0;
+ 
+void loop()
+{
+  unsigned int sensors[8];
+  
+  int position = qtr.readLine(sensors,QTR_EMITTERS_ON);
+ 
+  int error = int(position) - 3500;
+  integral += error;
+  derivative = error - lastError;
+  int power_difference = kp * error + ki * integral + kd * derivative;
+  Serial.println(power_difference);
+  lastError = error;
+  
+  const int maximum = 100;
+  
+  if (power_difference > maximum)
+    power_difference = maximum;
+  if (power_difference < -maximum)
+    power_difference = -maximum;  
+    
+  if (power_difference < 0) {
+    digitalWrite(rightMotorF, HIGH);
+    digitalWrite(rightMotorB, LOW);
+    analogWrite(rightMotorPWM, maximum);
+    digitalWrite(leftMotorF, HIGH);
+    digitalWrite(leftMotorB, LOW);
+    analogWrite(leftMotorPWM, maximum + power_difference);
+    digitalWrite(stby,HIGH);
+  } 
+  else { 
+    digitalWrite(rightMotorF, HIGH);
+    digitalWrite(rightMotorB, LOW);
+    analogWrite(rightMotorPWM, maximum - power_difference);
+    digitalWrite(leftMotorF, HIGH);
+    digitalWrite(leftMotorB, LOW);
+    analogWrite(leftMotorPWM, maximum);
+    digitalWrite(stby,HIGH);
+  }
+}
+
